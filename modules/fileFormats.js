7f179c884b776037d6ae861d5a9a913a9237451d
Alex Taylor <altays633@gmail.com>
2021-04-18 14:11:07 -0400

    modifying fileFormat switch statement to reduce repetition

--- modules/fileFormats.js
+++ modules/fileFormats.js
@@ -6,39 +6,34 @@ const raw = require('./raw');
 const wav = require('./wav');
 
 exports.generateData = (inputData, outputType, configObj) => {
-    // add config parameter above
-
-    // might need to copy the .wav format for the others and export bytes
-
-    // add config param to each of the generate/modify functions
-
     let header, newHex;
     switch(outputType){
         case 'mp3':
             header = mp3.header(inputData);
             newHex = mp3.hex(inputData);
-            return header+newHex;
+            break;
         case 'jpg':
             header = jpg.header(inputData);
             newHex = jpg.hex(inputData);
-            return header+newHex;
+            break;
         case 'wav':
             header = wav.header(inputData, configObj);
             newHex = wav.hex(inputData, configObj);
-            arr = [header,newHex];
-            buf = Buffer.concat(arr);
-            return buf;
+            break;
         case 'bmp':
             header = bmp.header(inputData);
             newHex = bmp.hex(inputData);
-            return header+newHex;
+            break;
         case 'gif':
             header = gif.header(inputData);
             newHex = gif.hex(inputData);
-            return header+newHex;
+            break;
         case 'raw':
             header = raw.header(inputData);
             newHex = raw.hex(inputData);
-            return header+newHex;
+            break;
     }
+    arr = [header,newHex];
+    buf = Buffer.concat(arr);
+    return buf;
 }
