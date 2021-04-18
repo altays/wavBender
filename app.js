c258b89022a679e45bb5438cef586bf9e9e44f0b
Alex Taylor <altays633@gmail.com>
2021-04-18 15:21:20 -0400

    adding functionality for scrambling input file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const util = require('util');
 const format = require('./modules/fileFormats');
 const utilities = require('./modules/utilities');
 const readFile = util.promisify(fs.readFile);
-const path = require('path')
+const path = require('path');
 
 // test string - node app.js testfile.txt outfile -f=w
 
@@ -11,36 +11,36 @@ function fileRead(file) {
     return readFile(file, 'utf8')
 }
 
-let formatType, bentFileName, header, newHex, glitchData;
-[nodePath, filePath,  readFileName, outputFileName, ...args] = process.argv;
+let formatType, bentFileName, glitchData;
+[nodePath, filePath, readFileName, outputFileName, ...args] = process.argv;
 
 fileRead(path.join("inputs/",readFileName)).then(data => {
-    const Buf = Buffer.from(data, "utf8").toString('hex');
-
+    
     configObj = new Object();
+    modData = data;
 
     args.forEach(arg => {
         let prefix = arg.trim().slice(0,2);
         let postfix = arg.trim().slice(3);
-        let params = {}
-        // if argument begins with "-c", run through config checking and return those values as an object
-            // example -> -c=width:200,height:200,channels:1
-            // returned object would be {width:200,height:200,channels: 1}
-            // then pass the config object into generateData()
         if (prefix == "-f") {
             formatType = utilities.checkFormatType(postfix);
         }
         if (prefix == "-c") {
             configObj = utilities.checkConfig(postfix);
         }
+        if (prefix == "-s") {
+            modData = utilities.inputScrambler(data);
+        }
     });
+
+    const Buf = Buffer.from(modData, "utf8").toString('hex');
 
     let versionName = utilities.nameVersion(outputFileName)
     bentFileName = `${versionName}.${formatType}`;
     glitchData = format.generateData(Buf, formatType, configObj);
 
     fs.writeFile(path.join("outputs/",formatType,"/",bentFileName), glitchData, (err) => {
-        if (err) console.log("error!");
+        if (err) console.log(err);
         console.log('The file has been saved!');
       });
 })
--- modules/utilities.js
+++ modules/utilities.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 exports.checkFormatType = (postfix) => {   
     switch(postfix){
         case 'm':
@@ -29,14 +32,61 @@ exports.checkConfig = (postfix) => {
     return tempConfigObj
 }
 
-exports.nameVersion = (outputName) => {
+nameVersion = (outputName) => {
     let dateAndTime = Date.now();
     return outputName + dateAndTime;
 }
 
-exports.scrambleInput = (inputFile, saveBool) => {
-    // read through input file
-        // scramble in different ways - shuffle, repeat random strings, etc.
-    // if saveBool is true, write the scrambled file in inputs 
-    // return scrambled input
+exports.nameVersion = nameVersion;
+
+// from MDN https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+scrambleText = (value, situ, repeats) => {
+    tempVal = value;
+    switch (situ) {
+        case 0:
+            tempVal = tempVal.toUpperCase();
+            tempVal = tempVal.repeat(repeats)
+            break;
+        case 1:
+            tempVal = tempVal.toLowerCase();
+            tempVal = tempVal.repeat(repeats)
+            break;
+        case 2:
+            tempVal = tempVal;
+            tempVal = tempVal.repeat(repeats)
+            break;
+    }
+    return tempVal;
+}
+
+exports.inputScrambler = (inputText) => {
+    let inputScrambled = "";
+    for (let i = 0; i < inputText.length; i++) {
+
+        let tempVal = inputText[i]
+        let value;
+        let randNums = [getRandomIntInclusive(1,2),getRandomIntInclusive(0,2),getRandomIntInclusive(2,10), getRandomIntInclusive(1,2)]
+
+        if (randNums[0] = 0) {
+            // skipping over this value
+            value = ""
+        } else if (randNums[0] = 1) {
+            // using the index
+            value = scrambleText(tempVal,randNums[1],randNums[3]);
+        } else {
+            tempVal = tempVal.slice(0,randNums[2]);
+            value = scrambleText(tempVal,randNums[1],randNums[3]);
+        }
+        inputScrambled += value;        
+    }
+
+    // console.log(inputScrambled)
+
+    return inputScrambled
 }
