3357f1e855db806be1dbceb9978892f1b55cc267
Alex Taylor <altays633@gmail.com>
2021-04-20 12:29:55 -0400

    now generating gif files that open in vs code. no pixels are displaying

--- modules/gif.js
+++ modules/gif.js
@@ -3,8 +3,9 @@
 // http://fileformats.archiveteam.org/wiki/GIF#Animated_GIF
 // going to want to use the 89a format - allows for animated gifs
 
+// issues:
+    // it is opening in vscode, but it is opening as a transparent gif
 
-// not sure why this isn't working :///////////
 exports.header = (inputData, configObj) => {
     let header, input, headerStr;
     let gifHead, logicalScreenDescriptor, cWidth, cHeight, packedField1, backgroundColorAspect, pixelAspectRatio;
@@ -12,15 +13,15 @@ exports.header = (inputData, configObj) => {
     let applicationExtension;
    
     gifHead = "474946383961" // "GIF89a"
-    cWidth = "0258" //600 pixels // might need to swap these values
-    cHeight = "0258" // 600 pixels // might need to swap these values *58 and 02
-    packedField1 = "FA" //global color table is present, 8 bits/pixel, decrasing importance, 8 colors in table
-    backgroundColorAspect = "00";
-    pixelAspectRatio = "00"
+    cWidth = "6400" //600 pixels // might need to swap these values
+    cHeight = "6400" // 600 pixels // might need to swap these values *58 and 02
+    packedField1 = "91" //global color table is present, 2 bits/pixel, decrasing importance, 2 colors in table
+    backgroundColorAspect = "FF";
+    pixelAspectRatio = "01"
     logicalScreenDescriptor = cWidth + cHeight + packedField1 + backgroundColorAspect + pixelAspectRatio;
 
-    globalColorTable = "" // 8 colors set in packedField1, need 3 hex values for RGB
-    for (let i = 0; i < 24; i++) {
+    globalColorTable = "" // 2 colors set in packedField1, need 3 hex values for RGB
+    for (let i = 0; i < 12; i++) {
         globalColorTable += inputData[i]
     }
 
@@ -43,40 +44,39 @@ exports.hex = (inputData, configObj) => {
     let imageData, lzwMinCodeSize, dataSubBlock, blockSize, blockTerminator, imageDataChunk;
     let trailer;
 
-    cWidth = "0258" //600 pixels // might need to swap these values
-    cHeight = "0258" // 600 pixels // might need to swap these values *58 and 02
+    cWidth = "6400" //600 pixels // might need to swap these values
+    cHeight = "6400" // 600 pixels // might need to swap these values *58 and 02
     dataChunkSize = 10 // use this to determine how many values get grabbed at a time
     imageData = "";
 
     for (let i = 30; i < inputData.length; i+= dataChunkSize) {
-        
-        // graphic control extension
-        extensionIntroducer = "21"
-        graphicControlLabel = "F9" 
-        byteSize = "04" 
-        packedField2 ="00" 
-        delayTime = "00" // this could be modified. need to translate numbers into hex
-        transparentColorIndex = "99" 
-        blockTerminator = "00"
-        graphicControlExtension = extensionIntroducer + graphicControlLabel + byteSize + packedField2 + delayTime + transparentColorIndex + blockTerminator;
+        if (inputData.slice(i,i+dataChunkSize).length == dataChunkSize) {
+            // graphic control extension
+            extensionIntroducer = "21"
+            graphicControlLabel = "F9" 
+            byteSize = "00" 
+            packedField2 ="00" 
+            delayTime = "01" // this could be modified. need to translate numbers into hex
+            transparentColorIndex = "55"  // this could be variable
+            blockTerminator = "00"
+            graphicControlExtension = extensionIntroducer + graphicControlLabel + byteSize + packedField2 + delayTime + transparentColorIndex + blockTerminator;
 
-        // image descriptor
-        imageSeparator = "2C"
-        imageLeft = "00"; // this could be modified. need ot translate numbers into hex
-        imageTop = "00"; // this could be modified. need ot translate numbers into hex
-        imageWidth = cWidth; // this could be modified. need ot translate numbers into hex
-        imageHeight = cHeight; // this could be modified. need ot translate numbers into hex
-        packedField3 = "00"
-        imageDescriptor = imageSeparator + imageLeft + imageTop + imageWidth + imageHeight + packedField3
+            // image descriptor
+            imageSeparator = "2C"
+            imageLeft = "01"; // this could be modified. need ot translate numbers into hex
+            imageTop = "01"; // this could be modified. need ot translate numbers into hex
+            imageWidth = cWidth; // this could be modified. need ot translate numbers into hex
+            imageHeight = cHeight; // this could be modified. need ot translate numbers into hex
+            packedField3 = "00"
+            imageDescriptor = imageSeparator + imageLeft + imageTop + imageWidth + imageHeight + packedField3
 
-
-        
-        lzwMinCodeSize = "03" //this might be variable?
-        imageDataChunk = inputData.slice(i,i+dataChunkSize).split("").map(c => c.charCodeAt(0).toString(16).padStart(2, "0")).join(""); // converting from utf to hex. thank stack overflow
-        
-        blockSize = imageDataChunk.length.toString(16) // hexadecimal version of the size of the block. so, this = dataSubBlock.toString(16)
-        dataSubBlock = graphicControlExtension + imageDescriptor + lzwMinCodeSize + blockSize + imageDataChunk + blockTerminator;
-        imageData += dataSubBlock;
+            lzwMinCodeSize = "01" //this might be variable -> from 0 to 9
+            imageDataChunk = inputData.slice(i,i+dataChunkSize).split("").map(c => c.charCodeAt(0).toString(16).padStart(2, "0")).join(""); // converting from utf to hex. thank stack overflow
+            // console.log(imageDataChunk)
+            blockSize = imageDataChunk.length.toString(16) // hexadecimal version of the size of the block. so, this = dataSubBlock.toString(16)
+            dataSubBlock = graphicControlExtension + imageDescriptor + lzwMinCodeSize + blockSize + imageDataChunk + blockTerminator;
+            imageData += dataSubBlock;
+        }
     }
 
     trailer = "3B"
