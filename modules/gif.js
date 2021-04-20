1f037c1e9f58f860408005ef6b62020d5e4334b3
Alex Taylor <altays633@gmail.com>
2021-04-20 12:07:53 -0400

    adding application extension - gifs are now opening, but are not displaying any data

--- modules/gif.js
+++ modules/gif.js
@@ -9,10 +9,11 @@ exports.header = (inputData, configObj) => {
     let header, input, headerStr;
     let gifHead, logicalScreenDescriptor, cWidth, cHeight, packedField1, backgroundColorAspect, pixelAspectRatio;
     let globalColorTable;
+    let applicationExtension;
    
     gifHead = "474946383961" // "GIF89a"
-    cWidth = "000A" //600 pixels // might need to swap these values
-    cHeight = "000A" // 600 pixels // might need to swap these values *58 and 02
+    cWidth = "0258" //600 pixels // might need to swap these values
+    cHeight = "0258" // 600 pixels // might need to swap these values *58 and 02
     packedField1 = "FA" //global color table is present, 8 bits/pixel, decrasing importance, 8 colors in table
     backgroundColorAspect = "00";
     pixelAspectRatio = "00"
@@ -25,8 +26,11 @@ exports.header = (inputData, configObj) => {
 
     globalColorTable = globalColorTable.split("").map(c => c.charCodeAt(0).toString(16).padStart(2, "0")).join(""); // converting from utf to hex. thank stack overflow
 
-    header = Buffer.alloc(gifHead.length + logicalScreenDescriptor.length + globalColorTable.length);
-    headerStr = gifHead + logicalScreenDescriptor + globalColorTable ;
+    // application extension
+    applicationExtension = "21FF0B4E45545343415045322E30"
+
+    header = Buffer.alloc(gifHead.length + logicalScreenDescriptor.length + globalColorTable.length + applicationExtension.length);
+    headerStr = gifHead + logicalScreenDescriptor + globalColorTable + applicationExtension;
     header = Buffer.from(headerStr, "hex")
     console.log(header.length)
     return header;
@@ -39,8 +43,8 @@ exports.hex = (inputData, configObj) => {
     let imageData, lzwMinCodeSize, dataSubBlock, blockSize, blockTerminator, imageDataChunk;
     let trailer;
 
-    cWidth = "000A" //600 pixels // might need to swap these values
-    cHeight = "000A" // 600 pixels // might need to swap these values *58 and 02
+    cWidth = "0258" //600 pixels // might need to swap these values
+    cHeight = "0258" // 600 pixels // might need to swap these values *58 and 02
     dataChunkSize = 10 // use this to determine how many values get grabbed at a time
     imageData = "";
 
@@ -64,6 +68,8 @@ exports.hex = (inputData, configObj) => {
         imageHeight = cHeight; // this could be modified. need ot translate numbers into hex
         packedField3 = "00"
         imageDescriptor = imageSeparator + imageLeft + imageTop + imageWidth + imageHeight + packedField3
+
+
         
         lzwMinCodeSize = "03" //this might be variable?
         imageDataChunk = inputData.slice(i,i+dataChunkSize).split("").map(c => c.charCodeAt(0).toString(16).padStart(2, "0")).join(""); // converting from utf to hex. thank stack overflow
