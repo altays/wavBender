e8a95f0752027ac0ac8bb1bbfe3b0ace240aea45
Alex Taylor <altays633@gmail.com>
2021-04-21 16:32:38 -0400

    renaming file, disabling config options, adding cli argument for text or hex input parsing

---
+++ convertFile.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const util = require('util');
+const format = require('./modules/fileFormats');
+const utilities = require('./modules/utilities');
+const readFile = util.promisify(fs.readFile);
+const path = require('path');
+
+// test string - node app.js testfile.txt outfile -f=w -t=t -s
+
+function fileRead(file) {
+    return readFile(file, 'utf8')
+}
+
+let formatType, bentFileName, glitchData, parseType;
+[nodePath, filePath, readFileName, outputFileName, ...args] = process.argv;
+
+fileRead(path.join("inputs/",readFileName)).then(data => {
+    
+    configObj = new Object();
+    modData = data;
+    parseType = "text"
+
+    args.forEach(arg => {
+        let prefix = arg.trim().slice(0,2);
+        let postfix = arg.trim().slice(3);
+        if (prefix == "-f") {
+            formatType = utilities.checkFormatType(postfix);
+        }
+        if (prefix == "-c") {
+            configObj = utilities.checkConfig(postfix);
+        }
+        if (prefix == "-s") {
+            modData = utilities.inputScrambler(data);
+        }
+        if (prefix == "-t") {
+            parseType = utilities.checkInputType(postfix);
+        }
+    });
+
+    let versionName = utilities.nameVersion(outputFileName)
+    bentFileName = `${versionName}.${formatType}`;
+
+    if (parseType == "text") {
+        glitchData = format.generateData(modData, formatType, configObj); 
+    } else if (parseType == "hex") {
+        glitchData = format.generateDataHex(modData, formatType, configObj)
+    }   
+
+    fs.writeFile(path.join("outputs/",formatType,"/",bentFileName), glitchData, (err) => {
+        if (err) console.log(err);
+        console.log('The file has been saved!');
+      });
+})
