d853242e34bca5a2202d1a566d2be3aa757c8234
Alex Taylor <altays633@gmail.com>
2021-05-31 13:47:31 -0400

    adding script to separate wav header from file, save rest of data as a seperate file

--- generateFile.js
+++ generateFile.js
@@ -16,12 +16,6 @@ function fileRead(file) {
 
 [nodePath, filePath, readFileName, ...args] = process.argv;
 
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-}
-
 fileRead(path.join('inputs/', readFileName)).then(data => {
     let formatType;
     let readFileNameNoExt = readFileName.slice(0,readFileName.length-4);
