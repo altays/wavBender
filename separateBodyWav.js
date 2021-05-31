5dc21a6cb6a52de51d590bf0b4fc3b7c1a6c080f
Alex Taylor <altays633@gmail.com>
2021-05-31 13:48:03 -0400

    actually adding the script to separate a header from a wav file

---
+++ separateBodyWav.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+const path = require('path');
+const utilities = require('./modules/utilities');
+
+// separates the header and body from a wav file, returns the bytes from the body
+
+// node separarateBodyWav glitchtest-wav.wavchars.txt
+
+function fileRead(file) {
+    return readFile(file, 'utf8')
+}
+
+[nodePath, filePath, readFileName, ...args] = process.argv;
+
+fileRead(path.join('inputs/', readFileName)).then(data => {
+    let readFileNameNoExt = readFileName.slice(0,readFileName.length-4);
+   
+    let body = data.slice(55,data.length)
+    let wavBody = Buffer.alloc(body.length)
+    wavBody.write(body, "utf16le")
+
+    let versionName = utilities.nameVersion(readFileNameNoExt)
+
+    fs.writeFile("inputs/" + readFileNameNoExt + versionName + ".txt", wavBody.toString('hex'), (err) => {
+        if (err) console.log(err);
+        else console.log('The file has been saved!');
+    });
+
+})
